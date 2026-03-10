11c873580aacf059c4d5e97ffce390fb3519f92d
Nasir <dev.nasiruddin@gmail.com>
2026-03-10 11:14:52 +0600

    add new feture

--- js/first.js
+++ js/first.js
@@ -1,63 +1,77 @@
 let input = document.querySelector('.input')
 let caption = document.querySelector('.caption')
 let postBtn = document.querySelector('.postBtn')
-let allPost = document.querySelector('.all-Post')
+let allPost = document.querySelector('.allPost')
+let updateBtn = document.querySelector('.updatebtn')
 
 let arr = []
+let editIndex;
 
-// postBtn.addEventListener("click",()=>{
-//   if(!input.value){
-//     input.placeholder="please Enter Something"
-//     input.style.border="1px solid red"
-//     return
-//   }else{
-//     if(!caption.value){
-//     caption.placeholder="Please Enter Something"
-//     caption.style.border="1px solid red"
-//     return
-//   }
-//   arr.push({
-//     input:input.value,
-//     caption:caption.value
-//   })
-//   allPost.innerHTML=""
-//   play()
-//   input.value=""
-//   caption.value=""
-
-// }})
-
-const abc = () => {
-  if (!input.value || !caption.value) {
-    input.placeholder = 'please Enter Something'
-    input.style.border = '1px solid red'
-    caption.placeholder = 'Please Enter Something'
-    caption.style.border = '1px solid red'
+postBtn.addEventListener('click', () => {
+  if (!input.value) {
+    input.style.border = '2px solid red'
+    input.placeholder = 'Pleasr enter something'
+  } else if (!caption.value) {
+    caption.style.border = '2px solid red'
+    caption.placeholder = 'Pleasr enter something'
   } else {
     arr.push({
       input: input.value,
       caption: caption.value,
     })
-    allPost.innerHTML = ''
-    play()
-    input.value = ''
-    caption.value = ''
   }
-}
+  allPost.innerHTML =""
+  play()
+  input.value =""
+  caption.value =""
+})
+
+
+updateBtn.addEventListener("click",()=>{
+  arr[editIndex].input=input.value
+  arr[editIndex].caption=caption.value
+
+  play()
+
+  updateBtn.style.display="none"
+  postBtn.style.display="block"
+
+  input.value=""
+  caption.value=""
+})
+
 
 function play() {
-  arr.map((item, index) => {
+  arr.map((item) => {
     allPost.innerHTML += `<div class="card">
       <h4>${item.input}</h4>
       <p>${item.caption}</p>
-      <button class="btn">Edit</button>
-      <button class="btn delete" onclick="deletePost(${index})">Delete</button>
+      <button class="edit">Edit</button>
+      <button class="delete">Delete</button>
     </div>`
+
+    let deleteBtn = document.querySelectorAll('.delete')
+    let convertDeleteBtn = Array.from(deleteBtn)
+    convertDeleteBtn.forEach((item, index) => {
+      item.addEventListener('click', () => {
+        arr.splice(index, 1)
+        allPost.innerHTML = ''
+        play()
+      })
+    })
+
+    let editBtn = document.querySelectorAll('.edit')
+    let convertEditBtn = Array.from(editBtn)
+    convertEditBtn.forEach((item, index) => {
+      item.addEventListener('click', () => {
+        postBtn.style.display = 'none'
+        updateBtn.style.display = 'block'
+
+
+
+        input.value = arr[index].input
+        caption.value = arr[index].caption
+      })
+    })
   })
-}
-const deletePost = (index) => {
-  // allPost.style.display = 'none'
-  arr.splice(index, 1) // remove only that element
-  allPost.innerHTML = ''
-  play()
 }
